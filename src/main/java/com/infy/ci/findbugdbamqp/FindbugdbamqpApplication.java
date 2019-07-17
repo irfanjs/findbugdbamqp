c84980791b4ac9c351916919f5f72db08688a345
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-17 18:12:04 +0530

    code modify

---
+++ src/main/java/com/infy/ci/findbugdbamqp/FindbugdbamqpApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.findbugdbamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindbugdbamqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindbugdbamqpApplication.class, args);
+	}
+
+}
